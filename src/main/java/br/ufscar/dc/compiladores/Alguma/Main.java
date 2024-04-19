aee0196bced4510e436e7c390e9fc87c87b58cb8
Matheus Videira <matheus.videira@estudante.ufscar.br>
2024-04-18 22:03:03 -0300

    pequenos ajustes

--- src/main/java/br/ufscar/dc/compiladores/Alguma/Main.java
+++ src/main/java/br/ufscar/dc/compiladores/Alguma/Main.java
@@ -1,4 +1,4 @@
-// importando as bibliotecas necessárias
+// importando as libs necessárias
 package br.ufscar.dc.compiladores.Alguma;
 
 import java.io.FileNotFoundException;
@@ -11,45 +11,43 @@ import org.antlr.v4.runtime.Token;
 
 public class Main {
     public static void main(String args[]) {
-
         try {
-            String arquivoSaida = args[1];
             CharStream stream = CharStreams.fromFileName(args[0]);
-            try (PrintWriter writer = new PrintWriter(arquivoSaida)) { //escrita arquivo
-                var lexico = new Alguma(stream); //define lexico como Alguma
-
+            String arquivoSaida = args[1];
+            try (PrintWriter writer = new PrintWriter(arquivoSaida)) { //arquivo de saida
+                var lexico = new Alguma(stream); //define o lexico
                 Token token = null;
 
-                //varrer os tokens, identificando-os e printando suas linhas lidas
+                //varrer os tokens, identificando-os e printando eles
                 loop:
                 while ((token = lexico.nextToken()).getType() != Token.EOF) {
                     String nomeToken = Alguma.VOCABULARY.getDisplayName(token.getType());
 
                     switch (nomeToken) {
-                        case "INVALIDO":  //caracter nao identificado/invalido
+                        case "INVALIDO":  //token nao reconhecido
                             writer.println("Linha " + token.getLine() + ": " + token.getText() + " - simbolo nao identificado");
-                            break loop;
-                        case "CADEIA_ABERTA":  //faltou o aspas
+                            break loop; // Abortar execucao, erro
+                        case "CADEIA_ABERTA":  //faltou aspas para fechar a cadeia
                             writer.println("Linha " + token.getLine() + ": cadeia literal nao fechada");
-                            break loop;
-                        case "COMENTARIO_ABERTO":  //comentario sem fim
+                            break loop; // Abortar execucao, erro
+                        case "COMENTARIO_ABERTO":  //faltou colchetes para fechar o comentario
                             writer.println("Linha " + token.getLine() + ": comentario nao fechado");
-                            break loop;
-                        case "PALAVRAS_CHAVE":  //token faz parte de uma palavra chave
+                            break loop; // Abortar execucao, erro
+                        case "PALAVRAS_CHAVE":  //token eh uma palavra chave
                         case "OP_LOGICO":  //token eh operador logico
                         case "OP_ARITMETICO":  //token eh operador aritmetico
                         case "OP_RELACIONAL":  //token eh operador relacional
                             writer.println("<'" + token.getText() + "','" + token.getText() + "'>");
                             break;
-                        default:  //caso token nao seja nada
+                        default:  //caso token nao seja nennhum dos acima
                             writer.println("<'" + token.getText() + "'," + nomeToken + ">");
                             break;
                     }
                 }
-            }catch(FileNotFoundException ex) { //erro qnd n encontra arquivo
+            }catch(FileNotFoundException ex) { // tratamento de excecao do output
                 System.err.println("O arquivo/diretório não existe:"+args[1]);
             }
-        } catch (IOException errado) { //caso nao consiga fazer nada, gera erro
+        } catch (IOException errado) { //tratamento de excecao do input
             System.err.println("Erro ao abrir arquivo:"+args[1]);
         }
     }
